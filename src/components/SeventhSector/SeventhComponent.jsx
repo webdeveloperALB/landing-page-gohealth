08b3a705478430932f39ab38a27d7f2e36330503
Kevin <kevikoltraka157@gmail.com>
2025-04-11 21:55:44 +0200

    changes

--- src/components/SeventhSector/SeventhComponent.jsx
+++ src/components/SeventhSector/SeventhComponent.jsx
@@ -8,10 +8,10 @@ const SeventhComponent = ({ className }) => {
         <div className="hero-content">
           <div className="video-container">
             <iframe
-              src="https://www.youtube.com/embed/dwkLZUuBm4A?autoplay=1&mute=1&loop=1&playlist=dwkLZUuBm4A&controls=0&showinfo=0&rel=0&modestbranding=1&fs=0&iv_load_policy=3"
+              src={`https://www.youtube.com/embed/dwkLZUuBm4A?autoplay=1&mute=1&loop=1&playlist=dwkLZUuBm4A&controls=0&modestbranding=1&fs=0&iv_load_policy=3&rel=0&disablekb=1&playsinline=1&cc_load_policy=0&widget_referrer=${encodeURIComponent(window.location.href)}`}
               title="Dental Clinic Video"
               frameBorder="0"
-              allow="autoplay; encrypted-media"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen={false}
               style={{
                 width: '100%',
